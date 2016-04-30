0265098a5f0c38186dee61b77717fa297eaa4d9e
David Jimenez <david.jimenez19@gmail.com>
2016-04-30 22:02:35 +0200

    implemented film and films endpoints, returning fake data

--- app/controllers/v1/films/film-controller.js
+++ app/controllers/v1/films/film-controller.js
@@ -1,13 +1,24 @@
+'use Strict';
 
 function FilmController() {
 }
 
 function get(req, res, next) {
-  res.status(200).json({ hello: 'world' });
+
+    var id = req.params.id,
+        film = {
+            'id': parseInt(id),
+            'name': 'film ' + id,
+            'description': 'description 1',
+            'year': 2001,
+            'genre': 'terror'
+        };
+
+    res.status(200).json(film);
 }
 
 FilmController.prototype = {
-  get: get
+    get: get
 };
 
 var filmsController = new FilmController();
--- app/controllers/v1/films/films-controller.js
+++ app/controllers/v1/films/films-controller.js
@@ -3,7 +3,23 @@ function FilmsController() {
 }
 
 function get(req, res, next) {
-  res.status(200).json({ hello: 'world2' });
+
+  var nPage = parseInt(req.query.page),
+      n = parseInt(req.query.records),
+      films = [];
+
+      for(var id = 0;  id < n; id++) {
+
+        films.push({
+          'id': parseInt(id),
+          'name': 'film ' + id,
+          'description': 'description '+id,
+          'year': 2001,
+          'genre': 'terror'
+        });
+      }
+
+  res.status(200).json(films);
 }
 
 FilmsController.prototype = {
